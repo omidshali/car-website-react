101979514343ac40df37c98107e5247cf190bebb
omid <o.shali01@gmail.com>
2024-07-11 10:24:10 +0330

    Added tanStak query

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
 import "./App.css";
+import { useQuery } from "@tanstack/react-query";
+const url = "https://freetestapi.com/api/v1/cars";
 
 function App() {
+  const fetchCars = async () => {
+    const data = await fetch(url).then((response) => response.json());
+    return data;
+  };
+  const { data, isPending, isError, error } = useQuery({
+    queryKey: ["cars"],
+    queryFn: fetchCars,
+  });
+  if (isPending) {
+    console.log("loadding");
+  }
+  if (isError) {
+    console.log("error");
+  }
+
+  console.log(data);
+
   return <div className="App">Car Website</div>;
 }
 
